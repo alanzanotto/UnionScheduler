160654e2602b599dc1a4adfc9d663e508b1bf72f
Alan Zanotto <alanz@fruitco.com>
2016-01-11 12:56:17 -0800

    first commit

---
+++ db_connection.php
@@ -0,0 +1,11 @@
+<?php
+//Database Connection File.
+$host = "localhost";//bctreeschedule.db.9982491.hostedresource.com
+$user = "bctreeschedule";//bctreeschedule
+$pssw = "greenSouls6#";
+$db = "bctreeschedule";//bctreeschedule
+$link = mysqli_connect($host, $user, $pssw,$db);
+
+
+
+?>
